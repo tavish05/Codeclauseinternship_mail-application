879312155122a9b4d94d417e9488a222f617581c
Tavish vijay <141062490+tavish05@users.noreply.github.com>
2024-04-05 00:38:42 +0530

    Create Login route.py

---
+++ Login route.py
@@ -0,0 +1,21 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(id=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('config_smtp'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
